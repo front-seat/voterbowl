c8af09967fe757b5041fdb02929a00b57e9475f0
Dave <davepeck@gmail.com>
2024-04-14 17:15:51 -0700

    Basic email normalization.

---
+++ server/utils/email.py
@@ -0,0 +1,41 @@
+import dataclasses
+
+
+@dataclasses.dataclass(frozen=True)
+class Domains:
+    """A set of email domains for a school."""
+
+    primary: str
+    aliases: tuple[str, ...]
+
+
+def normalize_email(
+    address: str,
+    tag: str | None = "+",
+    dots: bool = True,
+    domains: Domains | None = None,
+) -> str:
+    """
+    Normalize an email address.
+
+    - Remove leading and trailing whitespace
+    - Convert the address to lowercase
+    - If provided, remove the `tag` character (+) and everything after it
+    - If requested, remove dots (.) from the address
+    - If provided, replace the domain with the primary domain if it is an alias
+
+    You must have previously validated the email address.
+
+    This will certainly fail with technically valid cases like quoted strings,
+    comments, and internationalized domain names. It should suffice for
+    VoterBowl's purposes.
+    """
+    address = address.strip().lower()
+    local, domain = address.split("@", maxsplit=1)
+    if tag and tag in local:
+        local = local.split(tag, maxsplit=1)[0]
+    if dots:
+        local = local.replace(".", "")
+    if domains and domain in domains.aliases:
+        domain = domains.primary
+    return f"{local}@{domain}"
---
+++ server/utils/test_email.py
@@ -0,0 +1,71 @@
+import unittest
+
+from . import email as e
+
+
+class NormalizeEmailTestCase(unittest.TestCase):
+    """Test the normalize_email function."""
+
+    def test_simple(self):
+        """Test a simple email address."""
+        email = "test@example.com"
+        expected = "test@example.com"
+        result = e.normalize_email(email)
+        self.assertEqual(result, expected)
+
+    def test_whitespace(self):
+        """Test an email address with whitespace."""
+        email = " test@example.com\t"
+        expected = "test@example.com"
+        result = e.normalize_email(email)
+        self.assertEqual(result, expected)
+
+    def test_lowercase(self):
+        """Test an email address with uppercase characters."""
+        email = "tEst@Example.com"
+        expected = "test@example.com"
+        result = e.normalize_email(email)
+        self.assertEqual(result, expected)
+
+    def test_tag(self):
+        """Test an email address with a tag."""
+        email = "test+tag@example.com"
+        expected = "test@example.com"
+        result = e.normalize_email(email)
+        self.assertEqual(result, expected)
+
+    def test_custom_tag(self):
+        """Test an email address with a custom tag."""
+        email = "test-tag@example.com"
+        expected = "test@example.com"
+        result = e.normalize_email(email, tag="-")
+        self.assertEqual(result, expected)
+
+    def test_no_tag(self):
+        """Test an email address with no tag separation."""
+        email = "test+tag@example.com"
+        expected = "test+tag@example.com"
+        result = e.normalize_email(email, tag=None)
+        self.assertEqual(result, expected)
+
+    def test_dots(self):
+        """Test an email address with dots."""
+        email = "test.test@example.com"
+        expected = "testtest@example.com"
+        result = e.normalize_email(email)
+        self.assertEqual(result, expected)
+
+    def test_no_dots(self):
+        """Test an email address with no dot elimination."""
+        email = "test.test@example.com"
+        expected = "test.test@example.com"
+        result = e.normalize_email(email, dots=False)
+        self.assertEqual(result, expected)
+
+    def test_domain_aliases(self):
+        """Test an email address with domain aliases."""
+        email = "test@alias.example.com"
+        expected = "test@example.com"
+        domains = e.Domains("example.com", ("alias.example.com",))
+        result = e.normalize_email(email, domains=domains)
+        self.assertEqual(result, expected)
