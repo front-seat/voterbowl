19b682b33fb1203b371fe4c155e2e37d2fac218b
Dave <davepeck@gmail.com>
2024-04-17 17:18:51 -0700

    Some dope shizz is going down. PERMISSION DENIED.

--- server/vb/urls.py
+++ server/vb/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import check, home, school
+from .views import check, finish_check, home, school
 
 urlpatterns = [
+    path("<slug:slug>/check/finish/", finish_check, name="finish_check"),
     path("<slug:slug>/check/", check, name="check"),
     path("<slug:slug>/", school, name="school"),
     path("", home),
--- server/vb/views.py
+++ server/vb/views.py
@@ -1,6 +1,9 @@
+from django import forms
+from django.core.exceptions import PermissionDenied
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, render
-from django.views.decorators.http import require_GET
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_GET, require_POST
 
 from .models import School
 
@@ -28,4 +31,37 @@ def check(request: HttpRequest, slug: str) -> HttpResponse:
     current_contest = school.contests.current()
     return render(
         request, "check.dhtml", {"school": school, "current_contest": current_contest}
+    )
+
+
+class VerifyForm(forms.Form):
+    """Form for verifying a check."""
+
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+
+
+@require_POST
+@csrf_exempt  # CONSIDER: maybe use Django's CSRF protection even here?
+def finish_check(request: HttpRequest, slug: str) -> HttpResponse:
+    """Handle a check registration form submission."""
+    school = get_object_or_404(School, slug=slug)
+    current_contest = school.contests.current()
+    if not current_contest:
+        raise ValueError("No active contest TODO")
+    form = VerifyForm(request.POST)
+    if not form.is_valid():
+        raise PermissionDenied("Invalid form")
+
+    return render(
+        request,
+        "finish_check.dhtml",
+        {
+            "school": school,
+            "current_contest": current_contest,
+            "first_name": form.cleaned_data["first_name"],
+            "last_name": form.cleaned_data["last_name"],
+            "email": form.cleaned_data["email"],
+        },
     )
