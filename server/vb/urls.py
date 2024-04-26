e2f24ea1067bbea77c71ca9bb2ac3f903acb8b86
Dave <davepeck@gmail.com>
2024-04-26 15:34:12 -0700

    Name the "home" route.

--- server/vb/urls.py
+++ server/vb/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path("<slug:slug>/check/finish/", finish_check, name="finish_check"),
     path("<slug:slug>/check/", check, name="check"),
     path("<slug:slug>/", school, name="school"),
-    path("", home),
+    path("", home, name="home"),
 ]
